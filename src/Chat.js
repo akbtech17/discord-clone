62f5c970a0c53eb62f2b6479bcb5407c58e676c7
anshul bansal <akb.tech17@gmail.com>
2020-10-11 21:31:47 +0530

    integrated chat header

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Chat.css'
+import ChatHeader from './ChatHeader'
 
 function Chat() {
     return (
         <div className="chat">
-            <h2>Chat</h2>
+            <ChatHeader />
         </div>
     )
 }
